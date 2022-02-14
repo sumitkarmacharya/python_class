bf5d0a7be7270ccada798d48e1580f760f3b8679
Sumit Karmacharya <sumitkarmacharya@gmail.com>
2022-02-14 16:51:47 +0545

    remove_redundancy modified

--- remove_redundancy.py
+++ remove_redundancy.py
@@ -1,3 +1,12 @@
 #Remove duplicate using for loop
-# Hint: try nested for loop 
+# Hint: try nested for loop
 mylist = [1,4,2,5,2,1,4,9,7,5]
+def removeduplicate(a):
+    for i in mylist:
+        for x in range(1,len(mylist)-1):
+            if x==i:
+                mylist.sort()
+                mylist.remove(i)
+    return mylist
+print(removeduplicate(mylist))
+    
