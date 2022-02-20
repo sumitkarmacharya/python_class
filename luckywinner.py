4b934827c491e9debcb44759c82ed3c2e4e46bc7
Sumit Karmacharya <sumitkarmacharya@gmail.com>
2022-02-20 21:26:33 +0545

    luckywinner game added

---
+++ luckywinner.py
@@ -0,0 +1,15 @@
+import random
+print("If you can guess right lucky number three times you will be our lucky winner")
+i=0
+count=0
+while i<10:
+    num=int(input("Guess the number:"))
+    r=random.randint(1,10)
+    i+=1
+    if num==r:
+        count+=1
+    print("Lucky number:",r)
+if count==3:
+    print("Congratulation you are lucky winner")
+else:
+    print("Sorry try again later")
